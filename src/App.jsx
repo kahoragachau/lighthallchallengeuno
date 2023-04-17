271d1764868550ea66430fccaf426bb599ce853c
kahoragachau <joelkahoragachau6@gmail.com>
2023-04-17 23:11:00 +0300

    feat: Add localstorage to store values

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(parseInt(localStorage.getItem("count") || 0))
+  
+  console.log(parseInt(count))
+
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+// Watch for changes using the useEffect hook and set the localstorage key:value with the current 
+  useEffect(()=>{
+    localStorage.setItem("count", count)
+  },[count])
+  
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Counter</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={handleClick}>
+          count is {(count)}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
